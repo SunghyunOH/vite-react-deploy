635cc6a83b542d65d02b8c44905907741570fc1f
SungHyun Oh <lessbutbttr@gmail.com>
2023-11-02 20:14:04 +0900

    feat: react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Contact from './pages/Contact.jsx'
+
+const Router = createBrowserRouter([
+  {
+    path: "/vite-react-diploy/",
+    element: <App />,
+    children: [
+      {
+        path: "/vite-react-diploy/",
+        element: <Home />,
+      },
+      {
+        path: "/vite-react-diploy/contact",
+        element: <Contact />,
+      },
+    ]
+  }
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={Router} />
+  </React.StrictMode>
 )
